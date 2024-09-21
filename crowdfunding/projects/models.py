f0f998e250b3fc19eeb53bdcd93e84eacd8ebfa3
Simone Wight <sywight@gmail.com>
2024-09-21 12:15:17 +1000

    added all the newest files we worked on

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -9,3 +9,13 @@ class Project(models.Model):
    image = models.URLField()
    is_open = models.BooleanField()
    date_created = models.DateTimeField(auto_now_add=True)
+
+class Pledge(models.Model):
+   amount = models.IntegerField()
+   comment = models.CharField(max_length=200)
+   anonymous = models.BooleanField()
+   project = models.ForeignKey(
+      'Project',
+      on_delete=models.CASCADE,
+      related_name='pledges'
+   )
