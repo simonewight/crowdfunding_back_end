4a231794d5fe442d7139ce423224121567237bb0
Simone Wight <sywight@gmail.com>
2024-09-24 20:07:42 +1000

    added more bits

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -7,6 +7,7 @@ class PledgeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProjectSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = apps.get_model('projects.Project')
         fields = '__all__'
