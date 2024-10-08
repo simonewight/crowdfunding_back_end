333b4eb65385c15d869077cf7a14fca927e6c46d
Simone Wight <sywight@gmail.com>
2024-10-08 11:57:56 +1000

    added projects serializers, permissions and views

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -4,11 +4,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import status, permissions
 from django.http import Http404
+from .permissions import IsOwnerOrReadOnly
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         projects = Project.objects.all()
@@ -29,9 +32,16 @@ class ProjectList(APIView):
             )
 
 class ProjectDetail(APIView):
+    
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+        ]
+    
     def get_object(self, pk):
         try:
             project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
             return project
         except Project.DoesNotExist:
             raise Http404
@@ -40,6 +50,22 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 class PledgeList(APIView):
     def get(self, request):
