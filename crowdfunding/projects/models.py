855d69fcfcfe68de8d18d0da388602930a0cca9e
Simone Wight <sywight@gmail.com>
2024-12-15 21:46:43 +1000

    Added get_days_remaining method to Project model

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,69 +1,46 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from datetime import datetime
 
 class Project(models.Model):
-    CATEGORY_CHOICES = [
-        ('Technology', 'Technology'),
-        ('Arts', 'Arts'),
-        ('Film', 'Film'),
-        ('Games', 'Games'),
-        ('Music', 'Music'),
-        ('Food', 'Food'),
-        ('Publishing', 'Publishing'),
-        ('Fashion', 'Fashion'),
-        ('Design', 'Design'),
-        ('Other', 'Other'),
-    ]
-
     title = models.CharField(max_length=200)
     description = models.TextField()
     goal = models.IntegerField()
     image = models.URLField()
-    is_open = models.BooleanField(default=True)
+    is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
-    date_end = models.DateTimeField(null=True, blank=True)
+    date_end = models.DateField()
     owner = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
         related_name='owner_projects'
     )
-    category = models.CharField(
-        max_length=100,
-        choices=CATEGORY_CHOICES,
-        default='Technology'
-    )
+    category = models.CharField(max_length=100, null=True, blank=True)
 
-    @property
-    def total_pledges(self):
-        return self.project_pledges.aggregate(
-            total_pledges=models.Sum('amount')
-        )['total_pledges'] or 0
-
-    @property
-    def pledges_count(self):
-        return self.project_pledges.count()
-
-    def get_total_pledges(self):
-        return sum(pledge.amount for pledge in self.project_pledges.all())
-
-    def get_pledges_count(self):
-        return self.project_pledges.count()
-
-    def save(self, *args, **kwargs):
+    def get_days_remaining(self):
         if not self.date_end:
-            # Set default end date to 30 days from creation
-            self.date_end = timezone.now() + timezone.timedelta(days=30)
-        super().save(*args, **kwargs)
+            return 0
+        
+        # Convert date_end to datetime if it's just a date
+        if isinstance(self.date_end, datetime):
+            end_date = self.date_end
+        else:
+            end_date = datetime.combine(self.date_end, datetime.max.time())
+            
+        now = timezone.now()
+        difference = end_date - now
+        days_remaining = difference.days + (difference.seconds / 86400)
+        return max(0, round(days_remaining))
 
 class Pledge(models.Model):
     amount = models.IntegerField()
     comment = models.CharField(max_length=200)
     anonymous = models.BooleanField()
     project = models.ForeignKey(
-        Project,
+        'Project',
         on_delete=models.CASCADE,
-        related_name='project_pledges'
+        related_name='pledges'
     )
     supporter = models.ForeignKey(
         get_user_model(),
