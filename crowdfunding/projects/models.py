b11401e4556a34660a698cb0c8773031d2f91cf1
Simone Wight <sywight@gmail.com>
2024-12-15 21:10:28 +1000

    Updated Project model with correct categories and properties

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -4,11 +4,16 @@ from django.utils import timezone
 
 class Project(models.Model):
     CATEGORY_CHOICES = [
+        ('Technology', 'Technology'),
         ('Arts', 'Arts'),
-        ('Music', 'Music'),
-        ('Games', 'Games'),
-        ('Tech', 'Tech'),
         ('Film', 'Film'),
+        ('Games', 'Games'),
+        ('Music', 'Music'),
+        ('Food', 'Food'),
+        ('Publishing', 'Publishing'),
+        ('Fashion', 'Fashion'),
+        ('Design', 'Design'),
+        ('Other', 'Other'),
     ]
 
     title = models.CharField(max_length=200)
@@ -26,14 +31,18 @@ class Project(models.Model):
     category = models.CharField(
         max_length=100,
         choices=CATEGORY_CHOICES,
-        default='Tech'
+        default='Technology'
     )
 
-    def save(self, *args, **kwargs):
-        if not self.date_end:
-            # Set default end date to 30 days from creation
-            self.date_end = timezone.now() + timezone.timedelta(days=30)
-        super().save(*args, **kwargs)
+    @property
+    def total_pledges(self):
+        return self.project_pledges.aggregate(
+            total_pledges=models.Sum('amount')
+        )['total_pledges'] or 0
+
+    @property
+    def pledges_count(self):
+        return self.project_pledges.count()
 
     def get_total_pledges(self):
         return sum(pledge.amount for pledge in self.project_pledges.all())
@@ -41,13 +50,11 @@ class Project(models.Model):
     def get_pledges_count(self):
         return self.project_pledges.count()
 
-    def get_days_remaining(self):
-        if self.date_end:
-            now = timezone.now()
-            time_left = self.date_end - now
-            days_left = time_left.days
-            return max(0, days_left)  # Don't return negative days
-        return 0
+    def save(self, *args, **kwargs):
+        if not self.date_end:
+            # Set default end date to 30 days from creation
+            self.date_end = timezone.now() + timezone.timedelta(days=30)
+        super().save(*args, **kwargs)
 
 class Pledge(models.Model):
     amount = models.IntegerField()
